ff4066573c408799bbd1aa1c78eed1fc2f56fac6
tenax66 <hiyoshir+tenax66@gmail.com>
2023-12-28 23:30:40 +0900

    use socket.io

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.Server(app);
+const io = socketio(server);
 
 const PORT = process.env.PORT || 3000;
 
 // routing
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 // run
 server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  console.log(`new user connected: ${socket.id}`);
+
+  io.on("disconnect", () => {
+    console.log(`user disconnected: ${socket.id}`);
+  });
 });
